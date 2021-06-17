7e6f83d59372ac988accf39976464350febb10e8
UncleBigLu <1807814821@qq.com>
2021-06-17 23:23:01 +0800

    First commit

---
+++ src/test/java/com/suncaper/hotelmanagement/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suncaper.hotelmanagement.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
